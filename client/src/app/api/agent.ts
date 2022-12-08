62da82191ce94ec4cc21636b155c810bebbcc41d
Henke <70940165+92Hank@users.noreply.github.com>
2022-12-08 22:02:55 +0100

    added paging, sorting and filtering
    Now it is possible to filter the activities if you are a host, going or if you want to see all activities. You can also filter by date. If you go to profile and then click on Events you will see a tab menu where you can see future events, past events and events you are hosting.

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { store } from "../stores/store";
 import { history } from '../..';
 import { User, UserFormValues } from "../models/user";
 import { Photo, Profile, UserActivity } from "../models/profile";
+import { PaginatedResult } from "../models/pagination";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -23,6 +24,11 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async (response) => {
     await sleep(1000);
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>
+    }
     return response;
 }, (error: any) => {
     const { data, status, config } = error.response!;
@@ -75,7 +81,8 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>("/activities"),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', { params })
+        .then(responseBody),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>("/activities", activity),
     update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
