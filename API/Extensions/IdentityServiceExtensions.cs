87ef459f18108a2290388c3afd8c762ebd02c2c0
Henke <70940165+92Hank@users.noreply.github.com>
2022-12-09 16:23:12 +0100

    cleanup

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -62,46 +62,6 @@ namespace API.Extensions
             services.AddScoped<TokenService>();
 
             return services;
-            //.AddDefaultTokenProviders();
-
-            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
-
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //    .AddJwtBearer(opt =>
-            //    {
-            //        opt.TokenValidationParameters = new TokenValidationParameters
-            //        {
-            //            ValidateIssuerSigningKey = true,
-            //            IssuerSigningKey = key,
-            //            ValidateIssuer = false,
-            //            ValidateAudience = false,
-            //            ValidateLifetime = true,
-            //            ClockSkew = TimeSpan.Zero
-            //        };
-            //        opt.Events = new JwtBearerEvents
-            //        {
-            //            OnMessageReceived = context =>
-            //            {
-            //                var accessToken = context.Request.Query["access_token"];
-            //                var path = context.HttpContext.Request.Path;
-            //                if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
-            //                {
-            //                    context.Token = accessToken;
-            //                }
-            //                return Task.CompletedTask;
-            //            }
-            //        };
-            //    });
-
-            //services.AddAuthorization(opt =>
-            //{
-            //    opt.AddPolicy("IsActivityHost", policy =>
-            //    {
-            //        policy.Requirements.Add(new IsHostRequirement());
-            //    });
-            //});
-            //services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
-            //services.AddScoped<TokenService>();
         }
     }
 }
